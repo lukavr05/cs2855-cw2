c98362e404e53d4bc162e4f0ad08887403726f4a
lukavr05 <lvrooyen@yahoo.com>
2024-11-05 12:32:07 +0000

    added invoice table

--- createdb.sql
+++ createdb.sql
@@ -1,22 +1,36 @@
 CREATE TABLE item (
   item_id int primary key,
-  item_name varchar(20),
-  item_price numeric(12,2),
-  item_manufacturer varchar(20)
+  item_name varchar(20) not null,
+  item_price numeric(12,2) not null,
+  item_manufacturer varchar(20) not null
 );
 
 CREATE TABLE customer (
   customer_id int primary key,
-  customer_name varchar(20),
-  customer_address varchar(20)
+  customer_name varchar(20) not null,
+  customer_address varchar(20) not null
 );
 
 CREATE TABLE rating (
-  item_id int,
-  customer_id int,
-  rating_date date,
+  item_id int not null,
+  customer_id int not null,
+  rating_date date not null,
   rating_stars int,
   primary key(item_id, customer_id, rating_date),
   foreign key(item_id) references item,
-  foreign key(customer_id) references customer
+  foreign key(customer_id) references customer,
+  check (rating_stars >= 1),
+  check (rating_stars <= 5),
+);
+
+CREATE TABLE invoice (
+  invoice_id int primary key,
+  item_id int,
+  customer_id int,
+  item_quantity int,
+  total_cost numeric(12,2),
+  foreign key(item_id) references item,
+  foreign key(customer_id) references customer,
+  check (item_quantity > 0),
+  check (total_cost >= 0.00)
 );
