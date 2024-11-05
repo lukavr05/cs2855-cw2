067e3b51eadf4c41deba54019b8f186ecc479219
lukavr05 <lvrooyen@yahoo.com>
2024-11-05 12:19:20 +0000

    added customer and rating table

--- createdb.sql
+++ createdb.sql
@@ -1,6 +1,22 @@
 CREATE TABLE item (
   item_id int primary key,
   item_name varchar(20),
-  item_price float(10,2),
+  item_price float(12,2),
   item_manufacturer varchar(20)
+);
+
+CREATE TABLE customer (
+  customer_id int primary key,
+  customer_name varchar(20),
+  customer_address varchar(20)
+);
+
+CREATE TABLE rating (
+  item_id int,
+  customer_id int,
+  rating_date date,
+  rating_stars int,
+  primary key(item_id, customer_id, rating_date),
+  foreign key(item_id) references item,
+  foreign key(customer_id) references customer
 );
