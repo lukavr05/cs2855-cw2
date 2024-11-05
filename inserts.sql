478ce7887f7ff800627ece32be68e9d3fc2cee26
lukavr05 <lvrooyen@yahoo.com>
2024-11-05 13:22:29 +0000

    added rating inserts

--- inserts.sql
+++ inserts.sql
@@ -14,5 +14,13 @@ VALUES
     (14, 'David Brown', '101 Pine Rd'),
     (15, 'Eve Johnson', '202 Cedar Blvd');
 
-
+INSERT INTO rating (item_id, customer_id, rating_date, rating_stars)
+VALUES
+    (1, 11, '2023-01-01', 1),
+    (2, 12, '2021-10-22', 4),
+    (3, 13, '2014-03-13', 3),
+    (1, 14, '1950-07-04', 5),
+    (1, 11, '2023-03-01', 2), --- customer 1 rates item 11 again
+    (1, 11, '2023-02-01', 4), --- customer 1 rates item 11 again
+    (2, 15, '1991-11-15', 2);
 
