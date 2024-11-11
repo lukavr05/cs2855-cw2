31e1cd2e819417976b0fa53676bc0343ac363d63
lukavr05 <lvrooyen@yahoo.com>
2024-11-11 13:03:43 +0000

    fixed query3

--- query3.sql
+++ query3.sql
@@ -1,2 +1,2 @@
-SELECT item_name, max(rating_date), round(avg(rating_stars), 2) as average_rating FROM item , rating
+SELECT item.item_name, max(rating_date), round(avg(rating_stars), 2) as average_rating FROM item , rating
 ORDER BY average_rating DESC;
