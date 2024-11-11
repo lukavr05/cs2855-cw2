b6970d5cba9299b353e2316bdabb6a31426d78c8
lukavr05 <lvrooyen@yahoo.com>
2024-11-11 13:22:20 +0000

    fixed query3

--- query3.sql
+++ query3.sql
@@ -1,5 +1,5 @@
 SELECT (SELECT item_name FROM item WHERE item.item_id = r.item_id) as item_name,
-    max(r.rating_date),
+    max(r.rating_date) as most_recent_rating,
     round(avg(r.rating_stars), 2) as average_rating
 FROM rating r
 GROUP BY r.item_id
