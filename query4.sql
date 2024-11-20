97e5eb75061a20f1c42cdc6c0dc7b8a95657a570
lukavr05 <lvrooyen@yahoo.com>
2024-11-20 22:28:14 +0000

    added query4

---
+++ query4.sql
@@ -0,0 +1,27 @@
+SELECT 
+    c.customer_name,
+    i.item_name,
+    MAX(r.rating_stars) AS highest_star_rating
+FROM 
+    rating r
+JOIN 
+    customer c ON r.customer_id = c.customer_id
+JOIN 
+    item i ON r.item_id = i.item_id
+WHERE 
+    (r.item_id, r.customer_id) IN (
+        SELECT 
+            r1.item_id, 
+            r1.customer_id
+        FROM 
+            rating r1
+        GROUP BY 
+            r1.item_id, r1.customer_id
+        HAVING 
+            COUNT(*) > 1
+    )
+GROUP BY 
+    c.customer_name, i.item_name
+ORDER BY 
+    c.customer_name, i.item_name;
+
